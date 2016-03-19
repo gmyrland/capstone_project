b94ab71f47fbea2cbc8df7dcea333ba0468bd700
Glen Myrland <glen@myrland.com>
2016-03-18 20:34:16 -0400

    Modify read_db function to take table name as an argument

--- R/database.R
+++ R/database.R
@@ -5,11 +5,11 @@
 db <- function() {dbConnect(SQLite(), dbname="data/db.s3db")}
 
 ## Read dataframe from sqlite database
-read_db <- function() {
+read_db <- function(tbl) {
     db <- db()
     df <- NULL
     tryCatch(
-        df <- dbReadTable(db, "Data"),
+        df <- dbReadTable(db, name=tbl),
         error = function(cond) cat(cond$message),
         finally = dbDisconnect(db)
     )
